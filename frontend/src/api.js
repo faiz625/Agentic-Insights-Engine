900d1727a507018af2336b54fca419c200436a7c
faiz625 <faizkapadia264@gmail.com>
2025-10-21 12:40:04 -0400

    streamlined branch

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
-
-export async function runOrchestrator({ sql = null, limit = 1000 }) {
-  const { data } = await axios.post(`${API_BASE}/run`, { sql, limit });
-  return data;
-}
